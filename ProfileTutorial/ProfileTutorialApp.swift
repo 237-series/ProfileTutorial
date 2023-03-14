0cff82738ff70323888f4249dc6b2fff008af2bf
sglee237 <sglee237@gmail.com>
2023-03-14 09:26:05 +0900

    Initial Commit

---
+++ ProfileTutorial/ProfileTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileTutorialApp.swift
+//  ProfileTutorial
+//
+//  Created by sglee237 on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct ProfileTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
